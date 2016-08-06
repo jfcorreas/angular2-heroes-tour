c202c1dabcc5f54895e2793303c295b9fd5d8d1a
Juanfran Correas <jfcorreas@gmail.com>
2016-08-06 23:25:44 +0200

    Chapter 4: Multiple Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -69,7 +66,6 @@ const HEROES: Hero[] = [
     border-radius: 4px 0 0 4px;
   }
 `],
-
   template:`
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -81,15 +77,9 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-    `
+    <my-hero-detail [hero]="selectedHero" ></my-hero-detail>
+    `,
+  directives: [HeroDetailComponent]
 })
 
 export class AppComponent {
